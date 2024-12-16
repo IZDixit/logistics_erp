387bf9acec46c8a417a9dd17f9cd40a5bf1a9118
IZDixit <dixitimraan@gmail.com>
2024-12-16 15:10:51 +0300

    We have regitered all our models to the newly created superuser in the trakana db in postgresql

--- erp_main/erp/admin.py
+++ erp_main/erp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Job_Information, File_Ref_No, Client, Cargo_Type, Cargo_Classification, From_Location, To_Location, Route, Supplier_Information, Loose_Cargo_Information, Container_Details
 
 # Register your models here.
+# Register all my models to my admin account
+
+
+admin.site.register(Job_Information)
+admin.site.register(File_Ref_No)
+admin.site.register(Client)
+admin.site.register(Cargo_Type)
+admin.site.register(Cargo_Classification)
+admin.site.register(From_Location)
+admin.site.register(To_Location)
+admin.site.register(Route)
+admin.site.register(Supplier_Information)
+admin.site.register(Loose_Cargo_Information)
+admin.site.register(Container_Details)
